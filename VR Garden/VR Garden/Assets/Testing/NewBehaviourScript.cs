774c54a7ba5c47d4a3c49cc65c16e12bc0fab666
Abdullah Rowaished <37379928+AbdullahRowaishedVR@users.noreply.github.com>
2019-07-01 22:29:51 +0300

    Testing Shaders

---
+++ VR Garden/VR Garden/Assets/Testing/NewBehaviourScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+    private ComputeShader shader;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Respawn"))
+        {
+            shader.Dispatch(0, 0, 0, 0);
+        }
+    }
+}
