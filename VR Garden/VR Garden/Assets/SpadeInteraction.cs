d63be3c3c94a0094eccfbc6f8d81be109368ddf1
Abdullah Rowaished <37379928+AbdullahRowaishedVR@users.noreply.github.com>
2019-07-14 20:21:26 +0300

    SpadeInteraction and MockSpade testing
    MockSpade GameObject and SpadeTip child GameObject were created along with C# Script SpadeInteraction for Player to spawn Dirt piles.

---
+++ VR Garden/VR Garden/Assets/SpadeInteraction.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpadeInteraction : MonoBehaviour {
+    public GamePools gp;
+    private GameObject tip;
+
+    private void Start()
+    {
+        tip = GameObject.Find("SpadeTip");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Terrain"))
+        {
+            gp.PoolObject(tip.transform.position, "Dirt");
+        }
+    }
+}
