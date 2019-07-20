250e7711fe7f4da50d08e2d8fa9ae59f0d0ff837
Abdullah Rowaished <37379928+AbdullahRowaishedVR@users.noreply.github.com>
2019-07-20 21:14:33 +0300

    Watering Can
    watering can emits water particles when tilted at an angle.

--- VR Garden/VR Garden/Assets/Scripts/SpadeInteraction.cs
+++ VR Garden/VR Garden/Assets/Scripts/SpadeInteraction.cs
@@ -5,17 +5,28 @@ using UnityEngine;
 public class SpadeInteraction : MonoBehaviour {
     public GamePools gp;
     private GameObject tip;
+    private int shovelcount; //must shovel several times until a dirtpile shows up
 
     private void Start()
     {
         tip = GameObject.Find("SpadeTip");
+        shovelcount = 0;
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Terrain"))
         {
-            gp.PoolObject(tip.transform.position, "Dirt");
+            if (shovelcount == 10)
+            {
+                gp.PoolObject(tip.transform.position, "Dirt");
+                shovelcount = 0;
+            }
+            else
+            {
+                GetComponentInChildren<ParticleSystem>().Play();
+                shovelcount++;
+            }
         }
     }
 }
---
+++ VR Garden/VR Garden/Assets/Scripts/Watering.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Watering : MonoBehaviour {
+    private ParticleSystem waterPS;
+
+    private void Start()
+    {
+        waterPS = GameObject.Find("Water Particles").GetComponent<ParticleSystem>();
+    }
+
+    private void Update()
+    {
+        if (transform.eulerAngles.z > 60 && transform.eulerAngles.z < 180)
+        {
+            waterPS.Emit(1);
+        }
+    }
+}
