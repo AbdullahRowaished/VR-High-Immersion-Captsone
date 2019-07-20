c97dc76a45a37dad9a231e220574d8e01680ea61
Abdullah Rowaished <37379928+AbdullahRowaishedVR@users.noreply.github.com>
2019-07-20 11:32:08 +0300

    Scoring and Quality System
    Scoring System for game and Qualifier were added.

---
+++ VR Garden/VR Garden/Assets/Scoring.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scoring : MonoBehaviour {
+    private int totalScore;
+
+    private void Start()
+    {
+        totalScore = 0;
+    }
+
+    public int Score(Quality quality)
+    {
+        switch (quality)
+        {
+            case Quality.LOW:
+                totalScore += 10;
+                break;
+            case Quality.HIGH:
+                totalScore += 50;
+                break;
+        }
+
+        return totalScore;
+    }
+
+    public void ResetScore()
+    {
+        totalScore = 0;
+    }
+}
