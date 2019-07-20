e7d98c7c1a98d4524a7ad5c935e6aa138101cce6
Abdullah Rowaished <37379928+AbdullahRowaishedVR@users.noreply.github.com>
2019-07-20 13:52:11 +0300

    Quality and Scoring Scripts
    Quality Enum HIGH and LOW for produce and a Scoring System

---
+++ VR Garden/VR Garden/Assets/Scripts/GameLogic.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLogic : MonoBehaviour {
+    public Scoring scoreboard;
+
+    private List<GameObject> produceCollected;
+    private int fertilizerCount;
+
+    private void Start()
+    {
+        produceCollected = new List<GameObject>(24);
+        fertilizerCount = 30;
+    }
+
+
+}
