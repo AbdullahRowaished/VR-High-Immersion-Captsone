cf983e226027803655f38159e7e8bea0e6c69cf9
Abdullah Rowaished <37379928+AbdullahRowaishedVR@users.noreply.github.com>
2019-07-22 21:46:28 +0300

    Some changes

--- VR Garden/VR Garden/Assets/Scripts/DirtPile.cs
+++ VR Garden/VR Garden/Assets/Scripts/DirtPile.cs
@@ -6,13 +6,33 @@ public class DirtPile : MonoBehaviour {
     public bool used = false;
     public GameObject seed = null;
     public Produce produce;
+    public int waterAmount;
 
-    private void OnTriggerStay(Collider other)
+
+    private List<ParticleCollisionEvent> pcEvents;
+
+    private void Start()
+    {
+        pcEvents = new List<ParticleCollisionEvent>();
+        waterAmount = 0;
+    }
+
+    private void Update()
+    {
+        if (waterAmount >= 10000)
+        {
+            Debug.Log("plant has been watered");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
     {
         if (!used)
         {
             if (other.CompareTag("Eggplant") | other.CompareTag("Corn") | other.CompareTag("Pumpkin") | other.CompareTag("Tomato"))
             {
+                other.gameObject.GetComponent<Rigidbody>().isKinematic = true;
+                other.gameObject.transform.SetParent(transform);
                 seed = other.gameObject;
                 used = true;
             } else
@@ -32,6 +52,22 @@ public class DirtPile : MonoBehaviour {
             } else if (other.CompareTag("Tomato"))
             {
                 produce = Produce.TOMATO;
+            }
+        }
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        if (other.CompareTag("WaterCan"))
+        {
+            int numCollisionEvents = other.GetComponent<ParticleSystem>().GetCollisionEvents(gameObject, pcEvents);
+            int i = 0;
+
+            while (i < numCollisionEvents)
+            {
+                waterAmount++;
+                Debug.Log("WaterAmount: " + waterAmount);
+                i++;
             }
         }
     }
