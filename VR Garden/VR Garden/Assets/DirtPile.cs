447e5436bc06f7f920f341f4fc9d8f0f874babee
Abdullah Rowaished <37379928+AbdullahRowaishedVR@users.noreply.github.com>
2019-07-22 09:59:58 +0300

    Minor Changes

---
+++ VR Garden/VR Garden/Assets/DirtPile.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirtPile : MonoBehaviour {
+    private bool used = false;
+    GameObject plant = null;
+    private Produce produce;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (!used)
+        {
+            if (other.CompareTag("Eggplant") | other.CompareTag("Corn") | other.CompareTag("Pumpkin") | other.CompareTag("Tomato"))
+            {
+                plant = other.gameObject;
+                used = true;
+            }
+
+            if (other.CompareTag("Eggplant"))
+            {
+                produce = Produce.EGGPLANT;
+            } else if (other.CompareTag("Corn"))
+            {
+                produce = Produce.CORN;
+            } else if (other.CompareTag("Pumpkin"))
+            {
+                produce = Produce.PUMPKIN;
+            } else if (other.CompareTag("Tomato"))
+            {
+                produce = Produce.TOMATO;
+            }
+        }
+    }
+}
